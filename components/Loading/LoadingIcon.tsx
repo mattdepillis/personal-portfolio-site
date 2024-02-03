806c5551a488994177756aab001a123e37ebec10
mattdepillis <mattdepillis@gmail.com>
2024-02-03 14:12:53 -0500

    moved + refactored loading components

--- components/Loading/LoadingIcon.tsx
+++ components/Loading/LoadingIcon.tsx
@@ -1,11 +1,10 @@
-import * as React from 'react'
-
 import cs from 'classnames'
 
-import styles from './styles.module.css'
+import styles from '../styles.module.css'
 
 export const LoadingIcon = (props) => {
   const { className, ...rest } = props
+
   return (
     <svg
       className={cs(styles.loadingIcon, className)}
