fd391501c115130d07bf40b08e64bcc7a13a255d
mattdepillis <mattdepillis@gmail.com>
2024-02-08 11:23:43 -0500

    imports changed

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -3,8 +3,8 @@ import { NextRequest } from 'next/server'
 
 import { ImageResponse } from '@vercel/og'
 
-import { api, apiHost, rootNotionPageId } from '@/lib/config'
-import { NotionPageInfo } from '@/lib/types'
+import { api, apiHost, rootNotionPageId } from '@/lib/config/config'
+import { NotionPageInfo } from '@/lib/types/pages'
 
 const interRegularFontP = fetch(
   new URL('../../public/fonts/Inter-Regular.ttf', import.meta.url)
