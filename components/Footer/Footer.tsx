fb6a36301e69a4837150b2706e1041c5c070cf23
mattdepillis <mattdepillis@gmail.com>
2024-02-08 11:26:01 -0500

    footer alterations

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,24 @@
 // imports
 import { useCallback, useEffect, useState } from 'react'
+import classNames from 'classnames'
 
 // icons
+import { faPencil } from '@fortawesome/free-solid-svg-icons'
+import { faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { FaBriefcase } from '@react-icons/all-files/fa/FaBriefcase'
+import { FaCompactDisc } from '@react-icons/all-files/fa/FaCompactDisc'
 import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
 import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
+import { FaPalette } from '@react-icons/all-files/fa/FaPalette'
 import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter'
+
+import { IoFingerPrint } from '@react-icons/all-files/io5/IoFingerPrint'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
-import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+import { IoSunny } from '@react-icons/all-files/io5/IoSunny'
+
+import { MinusVertical } from 'tabler-icons-react'
 
 // config vars + dark mode support
 import { author, github, linkedin, twitter } from '@/lib/config/config'
@@ -18,7 +30,14 @@ import styles from '../styles.module.css'
 export const Footer: React.FC = () => {
   const [hasMounted, setHasMounted] = useState<boolean>(false)
   const { isDarkMode, toggleDarkMode } = useDarkMode()
-  const currentYear = new Date().getFullYear()
+
+  const footerClasses = classNames(styles.footer, {
+    [styles.footerDarkMode]: isDarkMode
+  })
+
+  const socialClasses = classNames(styles.pageRoute, {
+    [styles.darkModePageRoute]: isDarkMode
+  })
 
   const onToggleDarkMode = useCallback((e: any) => {
     e.preventDefault()
@@ -29,59 +48,125 @@ export const Footer: React.FC = () => {
     setHasMounted(true)
   }, [])
 
+
+
   return (
-    <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright {currentYear} {author}</div>
-
-      <div className={styles.settings}>
-        {hasMounted && (
-          <a
-            className={styles.toggleDarkMode}
-            href='#'
-            role='button'
-            onClick={onToggleDarkMode}
-            title='Toggle dark mode'
-          >
-            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
-          </a>
-        )}
-      </div>
-
+    <footer className={footerClasses}>
       <div className={styles.social}>
-        {twitter && (
-          <a
-            className={styles.twitter}
-            href={`https://twitter.com/${twitter}`}
-            title={`Twitter @${twitter}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaTwitter />
-          </a>
-        )}
+        {hasMounted && (
+          <>
+            <a
+            // TODO gotta change styling
+              className={socialClasses}
+              href='/about-me-9721bbd7b07e40769788bee7fe4e1566'
+              role='button'
+              title='About'
+            >
+              <IoFingerPrint />
+            </a>
 
-        {github && (
-          <a
-            className={styles.github}
-            href={`https://github.com/${github}`}
-            title={`GitHub @${github}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub />
-          </a>
-        )}
+            <a
+            // TODO gotta change styling
+              className={socialClasses}
+              href='/writing-1161cb246e014c0e9b0fbef369998df7'
+              role='button'
+              title='Writing'
+            >
+              <FontAwesomeIcon icon={faPencil} />
+            </a>
 
-        {linkedin && (
-          <a
-            className={styles.linkedin}
-            href={`https://www.linkedin.com/in/${linkedin}`}
-            title={`LinkedIn ${author}`}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaLinkedin />
-          </a>
+            <a
+            // TODO gotta change styling
+              className={socialClasses}
+              href='/projects-5265624e897544b892e70b3fd90044a5'
+              role='button'
+              title='Projects'
+            >
+              <FontAwesomeIcon icon={faScrewdriverWrench} />
+            </a>
+
+            <a
+            // TODO gotta change styling
+              className={socialClasses}
+              href='/career-09ca863431314490bf09e2ba27fed4d8'
+              role='button'
+              title='Resume'
+            >
+              <FaBriefcase />
+            </a>
+
+            <a
+              className={socialClasses}
+              href='/media-c07d64a7e5034b799d5c9c48a7400d21'
+              role='button'
+              title='Media'
+            >
+              <FaCompactDisc />
+            </a>
+
+            <a
+              className={socialClasses}
+              href='/art-a1ac252b0c4d410183337252ecaf5626'
+              role='button'
+              title='Art'
+            >
+              <FaPalette />
+            </a>
+
+            <a className={styles.socialDivider}>
+              <MinusVertical size={32} />
+            </a>
+
+            {twitter && (
+              <a
+                className={styles.twitter}
+                href={`https://twitter.com/${twitter}`}
+                title={`Twitter @${twitter}`}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaTwitter />
+              </a>
+            )}
+
+            {github && (
+              <a
+                className={styles.github}
+                href={`https://github.com/${github}`}
+                title={`GitHub @${github}`}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaGithub />
+              </a>
+            )}
+
+            {linkedin && (
+              <a
+                className={styles.linkedin}
+                href={`https://www.linkedin.com/in/${linkedin}`}
+                title={`LinkedIn ${author}`}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <FaLinkedin />
+              </a>
+            )}
+
+            {/* <a>
+              <MinusVertical />
+            </a> */}
+
+            <a
+              className={styles.toggleDarkMode}
+              href='#'
+              role='button'
+              onClick={onToggleDarkMode}
+              title='Toggle dark mode'
+            >
+              {isDarkMode ? <IoMoonSharp /> : <IoSunny />}
+            </a>
+          </>
         )}
       </div>
     </footer>
