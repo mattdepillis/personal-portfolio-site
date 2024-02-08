df5ada4f055e4d8266b9c914001d2526b17146bd
mattdepillis <mattdepillis@gmail.com>
2024-02-08 15:50:11 -0500

    dynamic route generation

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,22 +2,14 @@
 import { useCallback, useEffect, useState } from 'react'
 import classNames from 'classnames'
 
-// icons
-import { faPencil } from '@fortawesome/free-solid-svg-icons'
-import { faScrewdriverWrench } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import { FaBriefcase } from '@react-icons/all-files/fa/FaBriefcase'
-import { FaCompactDisc } from '@react-icons/all-files/fa/FaCompactDisc'
+// social icons
 import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
 import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
-import { FaPalette } from '@react-icons/all-files/fa/FaPalette'
 import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter'
-
-import { IoFingerPrint } from '@react-icons/all-files/io5/IoFingerPrint'
+// light + dark mode icons
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunny } from '@react-icons/all-files/io5/IoSunny'
-
+// divider icon
 import { MinusVertical } from 'tabler-icons-react'
 
 // config vars + dark mode support
@@ -27,15 +19,21 @@ import { useDarkMode } from '@/lib/utils/use-dark-mode'
 // styling
 import styles from '../styles.module.css'
 
+// page routes for dynamic rendering
+import { pageRoutes } from './pageRoutes'
+
+
 export const Footer: React.FC = () => {
   const [hasMounted, setHasMounted] = useState<boolean>(false)
   const { isDarkMode, toggleDarkMode } = useDarkMode()
 
+  // footer classnames dynamically created; dependent on dark mode
   const footerClasses = classNames(styles.footer, {
     [styles.footerDarkMode]: isDarkMode
   })
 
-  const socialClasses = classNames(styles.pageRoute, {
+  // page route classnames dynamically created; dependent on dark mode
+  const pageRouteClasses = classNames(styles.pageRoute, {
     [styles.darkModePageRoute]: isDarkMode
   })
 
@@ -49,69 +47,25 @@ export const Footer: React.FC = () => {
   }, [])
 
 
-
   return (
     <footer className={footerClasses}>
       <div className={styles.social}>
         {hasMounted && (
           <>
-            <a
-            // TODO gotta change styling
-              className={socialClasses}
-              href='/about-me-9721bbd7b07e40769788bee7fe4e1566'
-              role='button'
-              title='About'
-            >
-              <IoFingerPrint />
-            </a>
-
-            <a
-            // TODO gotta change styling
-              className={socialClasses}
-              href='/writing-1161cb246e014c0e9b0fbef369998df7'
-              role='button'
-              title='Writing'
-            >
-              <FontAwesomeIcon icon={faPencil} />
-            </a>
-
-            <a
-            // TODO gotta change styling
-              className={socialClasses}
-              href='/projects-5265624e897544b892e70b3fd90044a5'
-              role='button'
-              title='Projects'
-            >
-              <FontAwesomeIcon icon={faScrewdriverWrench} />
-            </a>
-
-            <a
-            // TODO gotta change styling
-              className={socialClasses}
-              href='/career-09ca863431314490bf09e2ba27fed4d8'
-              role='button'
-              title='Resume'
-            >
-              <FaBriefcase />
-            </a>
-
-            <a
-              className={socialClasses}
-              href='/media-c07d64a7e5034b799d5c9c48a7400d21'
-              role='button'
-              title='Media'
-            >
-              <FaCompactDisc />
-            </a>
-
-            <a
-              className={socialClasses}
-              href='/art-a1ac252b0c4d410183337252ecaf5626'
-              role='button'
-              title='Art'
-            >
-              <FaPalette />
-            </a>
+            {pageRoutes.map((route, i) => (
+              <a
+                key={i}
+                className={pageRouteClasses}
+                href={route.href}
+                role={route.role}
+                title={route.title}
+              >
+                {route.fontAwesomeIcon
+                  ? <route.fontAwesomeIcon icon={route.fontAwesomeIconType} />
+                  : <route.icon />
+                }
+              </a>
+            ))}
 
             <a className={styles.socialDivider}>
               <MinusVertical size={32} />
@@ -152,10 +106,6 @@ export const Footer: React.FC = () => {
                 <FaLinkedin />
               </a>
             )}
-
-            {/* <a>
-              <MinusVertical />
-            </a> */}
 
             <a
               className={styles.toggleDarkMode}
