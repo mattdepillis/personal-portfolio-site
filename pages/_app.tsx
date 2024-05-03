70c670cf3294c55b7f5187cae201e9d558ee16c2
mattdepillis <mattdepillis@gmail.com>
2024-05-03 17:20:23 -0400

    import changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,15 @@
 import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
-import 'styles/global.css'
+import '../styles/global.css'
 // this might be better for dark mode
 // import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
-import 'styles/notion.css'
+import '../styles/notion.css'
 // custom
-import 'styles/home.css'
-import 'styles/media.css'
+import '../styles/home.css'
 // global style overrides for prism theme (optional)
-import 'styles/prism-theme.css'
+import '../styles/prism-theme.css'
 
 import 'prismjs'
 import 'prismjs/components/prism-markup'
