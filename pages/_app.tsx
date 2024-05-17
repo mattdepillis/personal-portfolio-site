5bd1c346ee40736ed30076ce057cf9e54b064557
mattdepillis <mattdepillis@gmail.com>
2024-05-16 21:39:08 -0400

    changes to _app.tsx - mostly comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,32 @@
+// katex equation styling
+import 'katex/dist/katex.min.css'
+
 // used for code syntax highlighting (optional)
 import 'prismjs/themes/prism-coy.css'
-// core styles shared by all of react-notion-x (required)
-import 'react-notion-x/src/styles.css'
-import '../styles/global.css'
-// this might be better for dark mode
-// import 'prismjs/themes/prism-okaidia.css'
-// global style overrides for notion
-import '../styles/notion.css'
-// custom
-import '../styles/home.css'
+
 // global style overrides for prism theme (optional)
 import '../styles/prism-theme.css'
 
+// prismjs styles
 import 'prismjs'
 import 'prismjs/components/prism-markup'
 import 'prismjs/components/prism-javascript'
 import 'prismjs/components/prism-typescript'
 import 'prismjs/components/prism-python'
-import '../styles/prism-theme.css'
 
-// import 'rc-dropdown/assets/index.css'
-import 'katex/dist/katex.min.css'
+// core styles shared by all of react-notion-x (required)
+import 'react-notion-x/src/styles.css'
 
-// import 'react-static-tweets/styles.css'
+// global styles for starter kit site
+import '../styles/global.css'
 
-// * custom styles
-// import '../styles/globals.css'
-// import '../styles/custom-css/notion-collections.css'
-// import '../styles/custom-css/responsive-page-layout.css'
-// import '../styles/notion.css'
+// custom homepage styles
+import '../styles/home.css'
 
+// global style overrides for notion
+import '../styles/notion.css'
+
+// fontawesome config
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
